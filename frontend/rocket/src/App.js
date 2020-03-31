052f25b9b22db35b71674dbe33bb27980932c6c8
timothyleong97 <timothy.leong@u.nus.edu>
2020-03-31 16:35:19 +0800

    Add semantic ui react

--- frontend/rocket/src/App.js
+++ frontend/rocket/src/App.js
@@ -1,36 +1,47 @@
 import React from 'react';
-import './App.css';
-import faker from 'faker';
-import Comment from './components/Comment';
-import YesNoCard from './components/YesNoCard'
+import {Divider, Container} from 'semantic-ui-react';
+
 const App = () => {
   return (
-    <div className="ui container comments">
-      <YesNoCard>
-        <div>
-          <h4>Warning!</h4>
-          Are you sure you want to do this?
-        </div>
-      </YesNoCard>
-
-      <YesNoCard>
-        <Comment
-          author="Sam"
-          timeAgo="Today at 4:45PM"
-          content="Nice blog post"
-          avatar={faker.image.avatar()}
-        />
-      </YesNoCard>
-
-      <YesNoCard>
-        <Comment
-          author="Alex"
-          timeAgo="Today at 2:00AM"
-          content="I like the subject"
-          avatar={faker.image.avatar()}
-        />
-      </YesNoCard>
-    </div>
+    <Container style={{margin: 20}}>
+    <Container textAlign='left'>Left Aligned</Container>
+    <Container textAlign='center'>Center Aligned</Container>
+    <Container textAlign='right'>Right Aligned</Container>
+    <Container textAlign='justified'>
+      <b>Justified</b>
+      <Divider />
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+        quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+        Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+        dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
+        tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+        enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+        Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+        imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+        ultricies nisi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+        quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+        Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+        dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
+        tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+        enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
+        Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
+        imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+        ultricies nisi.
+      </p>
+    </Container>
+  </Container>
   );
 };
 

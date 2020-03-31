23b287638b25f3903253b75d7a813b8eef3c450c
timothyleong97 <timothy.leong@u.nus.edu>
2020-03-31 14:28:08 +0800

    created rocket react app

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/signup', (req, res)=> {
+    res.send({name:'timothy', age: 22})
+})
+
+app.get('/login', (req, res)=> {
+    res.send('Login page')
+})
+
+app.get('/order', (req, res)=> {
+    res.send('Order page')
+})
+
+app.get('/summary', (req, res)=> {
+    res.send('Summary page')
+})
+app.get('/schedule', (req, res)=> {
+    res.send('Schedule page')
+})
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+
+})
